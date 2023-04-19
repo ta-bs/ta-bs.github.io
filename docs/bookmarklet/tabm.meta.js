4d4f385051a5d2ad02276fbc9c77314a85d8e929
ta-bs <122689818+ta-bs@users.noreply.github.com>
2023-04-19 22:54:16 +0200

    add icon to userscript metadata

--- docs/bookmarklet/tabm.meta.js
+++ docs/bookmarklet/tabm.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name        Tiny Attack Bookmarklet
-// @version     1.0.1
-// @description Adds information to your Tiny Attack games.
+// @version     1.0.2
+// @description Adds information about the units of all players to your Tiny Attack games.
 // @author      Fex
 // @grant       none
 // @homepageURL https://ta-bs.github.io/
 // @downloadURL https://ta-bs.github.io/bookmarklet/tabm.user.js
 // @updateURL   https://ta-bs.github.io/bookmarklet/tabm.meta.js
+// @icon        https://ta-bs.github.io/bookmarklet/icon.png
 // @namespace   https://www.tinyattack.com/
 // @match       https://www.tinyattack.com/game/view.html?gameId=*
 // ==/UserScript==
