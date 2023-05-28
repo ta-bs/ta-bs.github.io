5d9b58096288b8001aec4f4236a73245813c7cbb
ta-bs <122689818+ta-bs@users.noreply.github.com>
2023-05-28 15:00:41 +0200

    rename query param to 'cache'

--- docs/bookmarklet/tabm.meta.js
+++ docs/bookmarklet/tabm.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Tiny Attack Bookmarklet
-// @version     1.1.0
+// @version     1.1.1
 // @description Adds information about the units of all players to your Tiny Attack games.
 // @author      Fex
 // @grant       none
